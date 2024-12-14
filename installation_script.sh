3610c6c173a0cbbb00030408393593cbd2a3f7b0
oDqnger <dimanchansilu@gmail.com>
2024-12-14 11:06:57 +0000

    fully completed installation scripts and executables

--- installation_script.sh
+++ installation_script.sh
@@ -1,8 +1,11 @@
-mkdir /usr/share/remindme
-mkdir /usr/share/remindme/music
+sudo mkdir /usr/share/remindme
+sudo mkdir /usr/share/remindme/music
 mkdir ~/.config/remindme/
-# curl config.toml file and put it in config folder
-# curl sample_music.mp3 and put it in usr/share/music folder
-# curl python_script and put it in usr/share/ folder
-# curl remindme sh script and put it in $PATH location
+curl -L -H "Accept: application/octet-stream" -o ~/.config/remindme/config.toml https://github.com/oDqnger/remindme/releases/download/test/config.toml
+sudo curl -L -H "Accept: application/octet-stream" -o /usr/share/remindme/music/testing.mp3 https://github.com/oDqnger/remindme/releases/download/test/testing.mp3
+sudo curl -L -H "Accept: application/octet-stream" -o /usr/share/remindme/remindme_python https://github.com/oDqnger/remindme/releases/download/test/remindme
+sudo chmod +x /usr/share/remindme/remindme_python
+sudo curl -L -H "Accept: application/octet-stream" -o /usr/local/bin/remindme https://github.com/oDqnger/remindme/releases/download/test/remindme_script
+sudo chmod +x /usr/local/bin/remindme
 
+echo "Installation completed"
