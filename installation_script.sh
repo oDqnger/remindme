aec1a162034e1c0d7db04b9c12c928ec9afb86db
oDqnger <dimanchansilu@gmail.com>
2024-12-14 16:46:40 +0000

    FINALLY DONE WITH DEPLOYMENT

--- installation_script.sh
+++ installation_script.sh
@@ -1,11 +1,11 @@
 sudo mkdir /usr/share/remindme
 sudo mkdir /usr/share/remindme/music
 mkdir ~/.config/remindme/
-curl -L -H "Accept: application/octet-stream" -o ~/.config/remindme/config.toml https://github.com/oDqnger/remindme/releases/download/test/config.toml
-sudo curl -L -H "Accept: application/octet-stream" -o /usr/share/remindme/music/testing.mp3 https://github.com/oDqnger/remindme/releases/download/test/testing.mp3
-sudo curl -L -H "Accept: application/octet-stream" -o /usr/share/remindme/remindme_python https://github.com/oDqnger/remindme/releases/download/test/remindme
+sudo curl -v -L -H "Accept: application/octet-stream" -o ~/.config/remindme/config.toml https://github.com/oDqnger/remindme/releases/download/beta_version/config.toml
+sudo curl -v -L -H "Accept: application/octet-stream" -o /usr/share/remindme/music/testing.mp3 https://github.com/oDqnger/remindme/releases/download/beta_version/testing.mp3
+sudo curl -v -L -H "Accept: application/octet-stream" -o /usr/share/remindme/remindme_python https://github.com/oDqnger/remindme/releases/download/beta_version/remindme
 sudo chmod +x /usr/share/remindme/remindme_python
-sudo curl -L -H "Accept: application/octet-stream" -o /usr/local/bin/remindme https://github.com/oDqnger/remindme/releases/download/test/remindme_script
+sudo curl -v -L -H "Accept: application/octet-stream" -o /usr/local/bin/remindme https://github.com/oDqnger/remindme/releases/download/beta_version/remindme_script
 sudo chmod +x /usr/local/bin/remindme
 
 echo "Installation completed"
